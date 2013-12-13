0eebd946bd385692456ba4ef12beffb6571d6cbe
Bibi <bibistrobel@googlemail.com>
2013-12-13 14:36:02 +0100

    first try

---
+++ gen/de/uniulm/bagception/retrieveuserposition/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.uniulm.bagception.retrieveuserposition;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
